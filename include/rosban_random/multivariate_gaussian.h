7ee13b7d5ab8ac6c6c5b8f5c77cfbb3d0b7a1082
medrimonia <medrimonia@gmail.com>
2017-08-17 23:17:08 +0200

    Importing multivariate_gaussian from Leph::Model and updating style

--- include/rosban_random/multivariate_gaussian.h
+++ include/rosban_random/multivariate_gaussian.h
@@ -1,39 +1,96 @@
+/// This file was imported from Quentin Rouxel PhD thesis repository
+/// with some style modifications
 #pragma once
 
+#include <vector>
+#include <random>
 #include <Eigen/Core>
 
-#include <random>
+namespace rosban_random {
 
-namespace rosban_random
-{
-
-class MultiVariateGaussian
+/// This class implement a Multivariate Gaussian distribution. It provides
+/// access to measures such as the density of probability at a given point
+/// 
+/// !!! Note that the current implementation for angles is not really the
+/// !!! wrapped normal distribution. Probability and fitting methods are
+/// !!! therefore quite wrong when the circular standard deviation is high
+/// !!! (around pi/2 or above)
+class MultivariateGaussian
 {
 public:
 
-  MultiVariateGaussian();
-  MultiVariateGaussian(const Eigen::VectorXd & mu,
-                       const Eigen::MatrixXd & covar);
+  /// Dummy empty initialization
+  MultivariateGaussian();
 
-  /// Draw a sample using the given random engine
-  Eigen::VectorXd getSample(std::default_random_engine & engine) const;
+  /// Initialization with mean vector and covariance matrix.  If optional
+  /// isCircular is not empty, each non zero value means that associated
+  /// dimension is an angle in radian.
+  MultivariateGaussian(const Eigen::VectorXd& mean,
+                       const Eigen::MatrixXd& covariance,
+                       const Eigen::VectorXi& isCircular = Eigen::VectorXi());
 
-  // Draw a given number of samples from the distribution
-  // Each column is a different sample
-  Eigen::MatrixXd getSamples(int nb_samples,
-                             std::default_random_engine & engine) const;
+  /// Return the gaussian 
+  /// dimentionality
+  size_t dimension() const;
 
-  void updateCholesky();
+  const Eigen::VectorXd& getMean() const;
+  const Eigen::MatrixXd& getCovariance() const;
+
+  /// Access to the vector specifiying for each dimension if it is circular
+  const Eigen::VectorXi& getCircularity() const;
+
+  /// Sample a vector from the multivariate gaussian with given random engine
+  Eigen::VectorXd sample(std::default_random_engine * engine) const;
+
+  /// Return the density of probability at 'point' given the distribution
+  /// parameters.
+  /// This method is very likely to return extremely small numbers, therefore
+  /// it is preferable to use getLogLikelihood
+  double getLikelihood(const Eigen::VectorXd& point) const;
+
+  /// Return the logarithm of the given point
+  double getLogLikelihood(const Eigen::VectorXd& point) const;
+
+  /// Compute the classic estimation of gaussian mean and covariance from given
+  /// data vectors.
+  /// If optional isCircular is not empty, each non zero value
+  /// means that associated dimension is an angle in radian.
+  void fit(const std::vector<Eigen::VectorXd>& points,
+           const Eigen::VectorXi& isCircular = Eigen::VectorXi());
 
 private:
-  /// The mean of the distribution
-  Eigen::VectorXd mu;
 
-  /// The covariance matrix
-  Eigen::MatrixXd covar;
+  /// The mean vector
+  Eigen::VectorXd _mean;
+  
+  /// The covariance matrix (symetrix definite positive)
+  Eigen::MatrixXd _covariance;
+  
+  /// Not null integer for each dimension where the represented value is an angle
+  /// in radian in [-pi,pi].
+  Eigen::VectorXi _isCircular;
 
-  /// The cholesky decomposition of the matrix
-  Eigen::MatrixXd cholesky;
+  /// Does the distribution has at least one circular dimension
+  bool _hasCircular;
+  
+  /// The inverse of covariance matrix computed through cholesky decomposition
+  Eigen::MatrixXd _covarianceInv;
+  
+  /// The left side of the Cholesky decomposition of the covariance matrix
+  Eigen::MatrixXd _choleskyDecomposition;
+  
+  /// The determinant of the 
+  /// covariance matrix
+  double _determinant;
+  
+  /// Compute the covariance decomposition and update internal variables
+  void computeDecomposition();
+  
+  /// Return the signed distance between
+  /// given point and current mean.
+  /// @throw logic_error if dimension of the point does not match dimension of
+  ///        the distribution
+  Eigen::VectorXd computeDistanceFromMean(const Eigen::VectorXd& point) const;
 };
 
 }
--- src/rosban_random/multivariate_gaussian.cpp
+++ src/rosban_random/multivariate_gaussian.cpp
@@ -1,50 +1,314 @@
 #include "rosban_random/multivariate_gaussian.h"
 
-#include <Eigen/Cholesky>
+#include <Eigen/Dense>
+
+#include <cmath>
+#include <stdexcept>
 
 namespace rosban_random
 {
 
-MultiVariateGaussian::MultiVariateGaussian()
+/// Return the angle corresponding in [-pi, pi]
+static double AngleBound(double angle)
 {
+  return angle - 2.0*M_PI*std::floor((angle + M_PI)/(2.0*M_PI));
 }
 
-MultiVariateGaussian::MultiVariateGaussian(const Eigen::VectorXd & mu_,
-                                           const Eigen::MatrixXd & covar_)
-  : mu(mu_), covar(covar_)
-{
-  updateCholesky();
-}
+/// Compute the oriented distance between the two given angle
+/// in the range -PI/2:PI/2 radian from angleSrc to angleDst
+/// (Better than doing angleDst-angleSrc)
+static double AngleDistance(double angleSrc, double angleDst) 
+{    
+  angleSrc = AngleBound(angleSrc);
+  angleDst = AngleBound(angleDst);
 
-Eigen::VectorXd MultiVariateGaussian::getSample(std::default_random_engine & engine) const
-{
-  Eigen::VectorXd random_vector(mu.rows());
-  std::normal_distribution<double> distribution(0,1);
-  for (int dim = 0; dim < mu.rows(); dim++)
-  {
-    random_vector(dim) = distribution(engine);
+  double max, min;
+  if (angleSrc > angleDst) {
+    max = angleSrc;
+    min = angleDst;
+  } else {
+    max = angleDst;
+    min = angleSrc;
   }
-  return mu + cholesky * random_vector;
-}
 
-Eigen::MatrixXd MultiVariateGaussian::getSamples(int nb_samples,
-                                                 std::default_random_engine & engine) const
-{
-  Eigen::MatrixXd result(mu.rows(), nb_samples);
-  for (int i = 0; i < nb_samples; i++) {
-    result.col(i) = getSample(engine);
+  double dist1 = max-min;
+  double dist2 = 2.0*M_PI - max + min;
+ 
+  if (dist1 < dist2) {
+    if (angleSrc > angleDst) {
+      return -dist1;
+    } else {
+      return dist1;
+    }
+  } else {
+    if (angleSrc > angleDst) {
+      return dist2;
+    } else {
+      return -dist2;
+    }
   }
-  return result;
 }
 
-void MultiVariateGaussian::updateCholesky()
+
+MultivariateGaussian::MultivariateGaussian() :
+  _mean(),
+  _covariance(),
+  _isCircular(),
+  _hasCircular(false),
+  _choleskyDecomposition(),
+  _determinant(0.0)
 {
-  //WARNING: according to Rasmussen 2006, it might be necessary to add epsilon * I
-  //         before computing cholesky
-  double epsilon = std::pow(10, -10);
-  Eigen::MatrixXd I;
-  I = Eigen::MatrixXd::Identity(covar.rows(), covar.rows());
-  cholesky = Eigen::LLT<Eigen::MatrixXd>(covar + epsilon * I).matrixL();
+}
+
+MultivariateGaussian::MultivariateGaussian(const Eigen::VectorXd& mean,
+                                           const Eigen::MatrixXd& covariance,
+                                           const Eigen::VectorXi& isCircular) :
+  _mean(mean),
+  _covariance(covariance),
+  _isCircular(),
+  _hasCircular(false),
+  _choleskyDecomposition(),
+  _determinant(0.0)
+{
+  //Check size
+  if (mean.size() != covariance.rows() ||
+      mean.size() != covariance.cols()) {
+    throw std::logic_error("MultivariateGaussian invalid input size");
+  }
+  if (isCircular.size() != 0 &&
+      isCircular.size() != mean.size()) {
+    throw std::logic_error("MultivariateGaussian invalid circular size");
+  }
+
+  //Circular initialization
+  if (isCircular.size() == mean.size()) {
+    _isCircular = isCircular;
+    //Normalization
+    for (size_t i=0;i<(size_t)_isCircular.size();i++) {
+      if (_isCircular(i) != 0) {
+        _isCircular(i) = 1;
+        _mean(i) = AngleBound(_mean(i));
+        _hasCircular = true;
+      }
+    }
+  } else {
+    _isCircular = Eigen::VectorXi::Zero(mean.size());
+    _hasCircular = false;
+  }
+
+  //Cholesky decomposition of covariance matrix
+  computeDecomposition();
+}
+        
+size_t MultivariateGaussian::dimension() const
+{
+  return _mean.size();
+}
+
+const Eigen::VectorXd& MultivariateGaussian::getMean() const
+{
+  return _mean;
+}
+const Eigen::MatrixXd& MultivariateGaussian::getCovariance() const
+{
+  return _covariance;
+}
+        
+const Eigen::VectorXi& MultivariateGaussian::getCircularity() const
+{
+  return _isCircular;
+}
+
+Eigen::VectorXd
+MultivariateGaussian::sample(std::default_random_engine * engine) const
+{
+  //Draw normal unit vector
+  Eigen::VectorXd unitRand(_mean.size());
+  std::normal_distribution<double> dist(0.0, 1.0);
+  for (size_t i=0;i<(size_t)unitRand.size();i++) {
+    unitRand(i) = dist(*engine);
+  }
+    
+  //Compute the random generated point
+  Eigen::VectorXd point = 
+    _mean + _choleskyDecomposition*unitRand;
+  //Angle normalization
+  if (_hasCircular) {
+    for (size_t i=0;i<(size_t)_isCircular.size();i++) {
+      if (_isCircular(i) != 0) {
+        point(i) = AngleBound(point(i));
+      }
+    }
+  }
+
+  return point;
+}
+
+double MultivariateGaussian::getLikelihood(const Eigen::VectorXd& point) const
+{
+  size_t size = _mean.size();
+  //Compute distance from mean
+  Eigen::VectorXd delta = computeDistanceFromMean(point);
+  // throw error if point is out of range
+  if (delta.size() == 0) {
+    throw std::logic_error("MultivariateGaussian: point is out of range");
+  }
+
+  //Compute likelihood
+  double tmp1 = -0.5*delta.transpose()*_covarianceInv*delta;
+  double tmp2 = pow(2.0*M_PI, size)*_determinant;
+  return std::exp(tmp1)/std::sqrt(tmp2);
+}
+
+double MultivariateGaussian::getLogLikelihood(const Eigen::VectorXd& point) const
+{
+  size_t size = _mean.size();
+  //Compute distance from mean
+  Eigen::VectorXd delta = computeDistanceFromMean(point);
+  // throw error if point is out of range
+  if (delta.size() == 0) {
+    throw std::logic_error("MultivariateGaussian: point is out of range");
+  }
+
+  //Compute log likelihood
+  double tmp1 = delta.transpose()*_covarianceInv*delta;
+  return -0.5*(std::log(_determinant) + tmp1 + (double)size*std::log(2.0*M_PI));
+}
+
+void MultivariateGaussian::fit(const std::vector<Eigen::VectorXd>& data,
+                               const Eigen::VectorXi& isCircular)
+{
+  //Check sizes
+  if (data.size() < 2) {
+    throw std::logic_error("MultivariateGaussian not enough data points");
+  }
+  size_t size = data.front().size();
+  for (size_t i=0;i<data.size();i++) {
+    if ((size_t)data[i].size() != size) {
+      throw std::logic_error("MultivariateGaussian invalid data dimension");
+    }
+  }
+  if (isCircular.size() != 0 && (size_t)isCircular.size() != size) {
+    throw std::logic_error("MultivariateGaussian invalid circular dimension");
+  } 
+    
+  //Circular initialization
+  _hasCircular = false;
+  if ((size_t)isCircular.size() == size) {
+    _isCircular = isCircular;
+    //Normalization
+    for (size_t i=0;i<(size_t)_isCircular.size();i++) {
+      if (_isCircular(i) != 0) {
+        _isCircular(i) = 1;
+        _hasCircular = true;
+      }
+    }
+  } else {
+    _isCircular = Eigen::VectorXi::Zero(size);
+  }
+
+  //Compute the mean estimation
+  if (_hasCircular) {
+    //If the dimension is circular,
+    //the cartesian mean (sumX, sumY) 
+    //is computed.
+    //Else, only sumX is used.
+    Eigen::VectorXd sumX = Eigen::VectorXd::Zero(size);
+    Eigen::VectorXd sumY = Eigen::VectorXd::Zero(size);
+    for (size_t i=0;i<data.size();i++) {
+      for (size_t j=0;j<size;j++) {
+        if (_isCircular(j) == 0) {
+          sumX(j) += data[i](j);
+        } else {
+          sumX(j) += std::cos(data[i](j));
+          sumY(j) += std::sin(data[i](j));
+        }
+      }
+    }
+    _mean = Eigen::VectorXd::Zero(size);
+    for (size_t j=0;j<size;j++) {
+      if (_isCircular(j) == 0) {
+        _mean(j) = (1.0/(double)data.size())*sumX(j);
+      } else {
+        double meanX = (1.0/(double)data.size())*sumX(j);
+        double meanY = (1.0/(double)data.size())*sumY(j);
+        _mean(j) = std::atan2(meanY, meanX);
+      }
+    }
+  } else {
+    //Standard non circular mean
+    Eigen::VectorXd sum = Eigen::VectorXd::Zero(size);
+    for (size_t i=0;i<data.size();i++) {
+      sum += data[i];
+    }
+    _mean = (1.0/(double)data.size())*sum;
+  }
+    
+  //Compute the covariance estimation
+  Eigen::MatrixXd sum2 = Eigen::MatrixXd::Zero(size, size);
+  for (size_t i=0;i<data.size();i++) {
+    Eigen::VectorXd delta = computeDistanceFromMean(data[i]);
+    sum2 += delta*delta.transpose();
+  }
+  _covariance = (1.0/(double)(data.size()-1))*sum2;
+
+  //Update the Cholesky decomposition
+  computeDecomposition();
+}
+        
+void MultivariateGaussian::computeDecomposition()
+{
+  //Add small epsilon on diagonal according 
+  //to Rasmussen 2006
+  double epsilon = 1e-9;
+  size_t size = _mean.size();
+  Eigen::MatrixXd noise = epsilon * Eigen::MatrixXd::Identity(size, size);
+  Eigen::LLT<Eigen::MatrixXd> llt(_covariance + noise);
+  _choleskyDecomposition = llt.matrixL();
+  //Check the decomposition
+  if(llt.info() == Eigen::NumericalIssue) {
+    throw std::logic_error("MultivariateGaussian Cholesky decomposition error");
+  }
+  //Compute the covariance determinant
+  _determinant = 1.0;
+  for (size_t i=0;i<(size_t)_mean.size();i++) {
+    _determinant *= _choleskyDecomposition(i, i);
+  }
+  _determinant = pow(_determinant, 2);
+  //Compute the covariance inverse
+  _covarianceInv = llt.solve(Eigen::MatrixXd::Identity(size, size));
+}
+
+Eigen::VectorXd
+MultivariateGaussian::computeDistanceFromMean(const Eigen::VectorXd& point) const
+{
+  size_t size = _mean.size();
+  if ((size_t)point.size() != size) {
+    throw std::logic_error(
+      "MultivariateGaussian: invalid dimension");
+  }
+
+  Eigen::VectorXd delta(size);
+  if (_hasCircular) {
+    for (size_t i=0;i<size;i++) {
+      if (_isCircular(i) == 0) {
+        //No circular distance
+        delta(i) = point(i) - _mean(i);
+      } else {
+        double angle = point(i);
+        //Outside of angle bounds
+        angle = AngleBound(angle);
+        //Compute circular distance
+        delta(i) = AngleDistance(_mean(i), angle);
+      }
+    }
+  } else {
+    //No circular distance
+    delta = point - _mean;
+  }
+
+  return delta;
 }
 
 }
+
